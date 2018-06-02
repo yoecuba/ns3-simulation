f83c80f70e562f9f25537941ab2ef97f157a4a37
Yoe <yhacaribe@gmail.com>
2018-06-02 14:21:46 -0500

    Changed2

--- script_test.bash
+++ script_test.bash
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-./waf --run "scratch/energy-model-example --pause=2 --nWifis=60 --CSVfileName="test/simulation2""
+./waf --run "scratch/energy-model-example --pause=2 --nWifis=30 --CSVfileName="test/simulation2""
 
 
 
-./waf --run "scratch/energy-model-example --pause=4 --nWifis=60 --CSVfileName="test/simulation5""
+./waf --run "scratch/energy-model-example --pause=4 --nWifis=30 --CSVfileName="test/simulation5""
 
 
 
-./waf --run "scratch/energy-model-example --pause=6 --nWifis=60 --CSVfileName="test/simulation8""
+./waf --run "scratch/energy-model-example --pause=6 --nWifis=30 --CSVfileName="test/simulation8""
 
 
-./waf --run "scratch/energy-model-example --pause=2 --nWifis=20 --CSVfileName="test/simulation0""
+./waf --run "scratch/energy-model-example --pause=2 --nWifis=10 --CSVfileName="test/simulation0""
 
-./waf --run "scratch/energy-model-example --pause=2 --nWifis=40 --CSVfileName="test/simulation1""
+./waf --run "scratch/energy-model-example --pause=2 --nWifis=20 --CSVfileName="test/simulation1""
 
-./waf --run "scratch/energy-model-example --pause=4 --nWifis=20 --CSVfileName="test/simulation3""
+./waf --run "scratch/energy-model-example --pause=4 --nWifis=10 --CSVfileName="test/simulation3""
 
-./waf --run "scratch/energy-model-example --pause=4 --nWifis=40 --CSVfileName="test/simulation4""
+./waf --run "scratch/energy-model-example --pause=4 --nWifis=20 --CSVfileName="test/simulation4""
 
-./waf --run "scratch/energy-model-example --pause=6 --nWifis=20 --CSVfileName="test/simulation6""
+./waf --run "scratch/energy-model-example --pause=6 --nWifis=10 --CSVfileName="test/simulation6""
 
-./waf --run "scratch/energy-model-example --pause=6 --nWifis=40 --CSVfileName="test/simulation7""
+./waf --run "scratch/energy-model-example --pause=6 --nWifis=20 --CSVfileName="test/simulation7""
