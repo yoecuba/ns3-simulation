2456723b305e1b916b9f4c76bc9c3ed05663ef2d
Yoe <yhacaribe@gmail.com>
2018-06-02 23:31:00 -0500

    editing

--- script_test.bash
+++ script_test.bash
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-./waf --run "scratch/energy-model-example --pause=2 --nWifis=60 --CSVfileName="/home/claudia/workspace/bake/source/ns-3.28/testing/simulation2""
+./waf --run "scratch/energy-model-example --pause=2 --nWifis=20 --CSVfileName="/testing/simulation0""
 
-./waf --run "scratch/energy-model-example --pause=4 --nWifis=60 --CSVfileName="/home/claudia/workspace/bake/source/ns-3.28/testing/simulation5""
+./waf --run "scratch/energy-model-example --pause=2 --nWifis=40 --CSVfileName="/testing/simulation1""
 
-./waf --run "scratch/energy-model-example --pause=6 --nWifis=60 --CSVfileName="/home/claudia/workspace/bake/source/ns-3.28/testing/simulation8""
+./waf --run "scratch/energy-model-example --pause=2 --nWifis=60 --CSVfileName="/testing/simulation2""
+
+./waf --run "scratch/energy-model-example --pause=4 --nWifis=20 --CSVfileName="/testing/simulation3""
+
+./waf --run "scratch/energy-model-example --pause=4 --nWifis=40 --CSVfileName="/testing/simulation4""
+
+./waf --run "scratch/energy-model-example --pause=4 --nWifis=60 --CSVfileName="/testing/simulation5""
+
+./waf --run "scratch/energy-model-example --pause=6 --nWifis=20 --CSVfileName="/testing/simulation6""
+
+./waf --run "scratch/energy-model-example --pause=6 --nWifis=40 --CSVfileName="/testing/simulation7""
+
+./waf --run "scratch/energy-model-example --pause=6 --nWifis=60 --CSVfileName="/testing/simulation8""
 
 
-./waf --run "scratch/energy-model-example --pause=2 --nWifis=20 --CSVfileName="/home/claudia/workspace/bake/source/ns-3.28/testing/simulation0""
+#4 con 1
+#1 con 3
+#6 con 4
+#3 con 6
 
-./waf --run "scratch/energy-model-example --pause=4 --nWifis=20 --CSVfileName="/home/claudia/workspace/bake/source/ns-3.28/testing/simulation1""
 
-./waf --run "scratch/energy-model-example --pause=6 --nWifis=20 --CSVfileName="/home/claudia/workspace/bake/source/ns-3.28/testing/simulation3""
-
-./waf --run "scratch/energy-model-example --pause=2 --nWifis=40 --CSVfileName="/home/claudia/workspace/bake/source/ns-3.28/testing/simulation4""
-
-./waf --run "scratch/energy-model-example --pause=4 --nWifis=40 --CSVfileName="/home/claudia/workspace/bake/source/ns-3.28/testing/simulation6""
-
-./waf --run "scratch/energy-model-example --pause=6 --nWifis=40 --CSVfileName="/home/claudia/workspace/bake/source/ns-3.28/testing/simulation7""
+ 
